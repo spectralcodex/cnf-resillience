f6cd85c4f27426c84dc493e8283316c0962f347c
Steph Freeman <stephen.adarkwah@nice.org.uk>
2026-05-30 23:48:43 +0100

    added template

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return JSONResponse(content=jsonable_encoder({"message": "Hello Bigger Applications!"}))
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from cnf-resillience!")
-
-
-if __name__ == "__main__":
-    main()
